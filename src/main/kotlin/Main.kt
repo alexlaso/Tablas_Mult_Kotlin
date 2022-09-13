d50d03089788d1227682b0fd43daa07db0056d70
alexlaso <alex_laso_laso@hotmail.com>
2022-09-13 20:27:48 +0200

    arreglo de orden en introducción de valores, añadida posibilidad de introducir valor mayor primero

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,23 @@
 fun main() {
     val intervaloTabla = 1..10
-    println("Por favor introduce el número mínimo del intevalo")
+    println("Por favor introduce el primer número del intevalo")
     val num1: Int = readln().toInt()
-    var indice: Int = num1
-    println("Por favor introduce el número máximo del intevalo")
+    println("Por favor introduce el segundo número del intevalo")
     val num2: Int = readln().toInt()
-    println("El menor es $num1 y el mayor es $num2")
-    val valoresRecibidos = num1..num2
+    val max: Int?
+    val min: Int?
+    if (num1 > num2) {
+        max = num1
+    } else {
+        max=num2
+    }
+    if (num1 < num2) {
+        min = num1
+    }else{
+        min= num2
+    }
+    var indice: Int = num1
+    val valoresRecibidos = min..max
 
     for (indice in valoresRecibidos){
         println("Multiplos de $indice: ")
